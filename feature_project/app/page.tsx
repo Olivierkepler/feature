fac832872b52f68cb414557ee58919cdceb407d1
Olivier Kepler Francois <98571148+Olivierkepler@users.noreply.github.com>
2026-06-07 22:03:41 +0000

    update

--- feature_project/app/page.tsx
+++ feature_project/app/page.tsx
@@ -6,12 +6,6 @@ export default function Home() {
     <div className="flex flex-col ">
 
 
-<Image        src="/images/papa_johns_logo.png"
-        alt="Papa John's Logo"
-        width={200}
-        height={100}
-        className="mb-6"
-      />
 
      <PapaJohnsAdCard
         imageUrl="/feature/images/how_to_wash_towels.jpg"
