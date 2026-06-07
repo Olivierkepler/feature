f5dae533e1d59e3f66839eab44da42e8b97fe54b
Olivier Kepler Francois <98571148+Olivierkepler@users.noreply.github.com>
2026-06-07 21:54:46 +0000

    update

--- feature_project/app/page.tsx
+++ feature_project/app/page.tsx
@@ -4,6 +4,15 @@ import PapaJohnsAdCard from "./components/PapaJohnsAdCard";
 export default function Home() {
   return (
     <div className="flex flex-col ">
+
+
+<Image        src="/images/papa_johns_logo.png"
+        alt="Papa John's Logo"
+        width={200}
+        height={100}
+        className="mb-6"
+      />
+
     <PapaJohnsAdCard
         imageUrl="/images/how_to_wash_towels.jpg" 
       />
